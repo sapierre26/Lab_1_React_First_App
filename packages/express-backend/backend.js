e2152a27419dbea9e47c6a486985a5cf5ce2ceea
Sanaia <sgpierre2003@gmail.com>
2025-01-14 11:20:39 -0800

    Step 5 completed

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,28 +4,28 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {                                                                             id: "yat999",                                                               name: "Dee",                                                                job: "Aspring actress"                                                    },                                                                          {
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(
+    `Example app listening at http://localhost:${port}`
+  );
+});
+
+const users = {                                                               users_list: [                                                                 {                                                                             id: "xyz789",                                                               name: "Charlie",                                                            job: "Janitor"                                                            },                                                                          {                                                                             id: "abc123",                                                               name: "Mac",                                                                job: "Bouncer"                                                            },                                                                          {                                                                             id: "ppp222",                                                               name: "Mac",                                                                job: "Professor"
+    },                                                                          {                                                                             id: "yat999",                                                               name: "Dee",                                                                job: "Aspring actress"                                                    },                                                                          {
       id: "zap555",
       name: "Dennis",                                                             job: "Bartender"
     }                                                                         ]
 };
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
 
 const findUserByName = (name) => {
   return users["users_list"].filter(
@@ -43,18 +43,15 @@ app.get("/users", (req, res) => {
   }
 });
 
-app.use(express.json());
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/users", (req, res) => {
-  res.send(users);
-});
-
-app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
