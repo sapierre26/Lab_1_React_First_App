12b9b9ebe46473c4844f1b18abbf6e64a53e27e8
Sanaia <sgpierre2003@gmail.com>
2025-01-21 22:56:54 -0800

    Step 3 completed

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -38,6 +38,26 @@ function MyApp() {
 	  .then((json) => setCharacters(json["users_list"]))
 	  .catch((error) => { console.log(error); });
   }, [] );
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+   }
+
+   function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
+   }
 }
 
 export default MyApp;
